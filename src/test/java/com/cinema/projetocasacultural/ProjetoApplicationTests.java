9798915afa51b7fc21f840727644c1ba7c81faf6
Lucas Tomaz <olucastomaz@hotmail.com>
2025-04-24 23:01:37 -0300

    update

---
+++ src/test/java/com/cinema/projetocasacultural/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.projetocasacultural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocasaculturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
